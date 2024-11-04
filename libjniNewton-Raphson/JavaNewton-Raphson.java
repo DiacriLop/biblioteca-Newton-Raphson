e6243abf39811b591269cdac90389296b96d88c6
Ubuntu <ubuntu@ip-172-31-60-34.ec2.internal>
2024-11-04 23:52:52 +0000

    realizamos el archivo que representa nuestra biblioteca en Java. creacion del archivo JavaNewton-Raphson.java

---
+++ libjniNewton-Raphson/JavaNewton-Raphson.java
@@ -0,0 +1,8 @@
+package libjniNewton-Raphson;
+
+public JavaNewton-Raphson {
+
+	public native ejecutarNewtonRaphson(int opcion, double x0, int max_iter, double tol);
+	// no implemento codigo ya que es un metodo nativo 
+
+}
