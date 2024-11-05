662394e26b4b060abb141b2f2bce10d796b3a2b8
DiacriLop <dianacristinaacdc12@gmail.com>
2024-11-05 00:48:16 +0000

    se modifico el .java se eliminaron los .h .c .class y se volvieron a generar con las correcciones del .java

--- libjniNewtonRaphson/JavaNewtonRaphson.java
+++ libjniNewtonRaphson/JavaNewtonRaphson.java
@@ -1,6 +1,19 @@
 package libjniNewtonRaphson;
 
 public class JavaNewtonRaphson {
+     static {
+        System.loadLibrary("newtonraphson"); // Carga la biblioteca nativa llamada 'newtonraphson'
+    }
+
+    // Método nativo para ejecutar el método de Newton-Raphson en una función seleccionada
     public native void ejecutarNewtonRaphson(int opcion, double x0, int maxIter, double tol);
-    // No implemento código ya que es un método nativo
+
+    public static void main(String[] args) {
+        JavaNewtonRaphson newtonRaphson = new JavaNewtonRaphson();
+        int opcion = 1;
+        double x0 = 1.0;
+        int maxIter = 100;
+        double tol = 1e-6;
+        newtonRaphson.ejecutarNewtonRaphson(opcion, x0, maxIter, tol);
+    }
 }
