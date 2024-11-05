8cc15e85a4a57c7ebb6e5db9de6dfade2f1eb620
DiacriLop <dianacristinaacdc12@gmail.com>
2024-11-05 00:18:27 +0000

    correccion de errores de sintaxis ya que no se permite el uso sel - en algunos casos.Tambien se generaron los archivosJavaNewtonRaphson.class   libjniNewtonRaphson_JavaNewtonRaphson.h

---
+++ libjniNewtonRaphson/JavaNewtonRaphson.java
@@ -0,0 +1,6 @@
+package libjniNewtonRaphson;
+
+public class JavaNewtonRaphson {
+    public native void ejecutarNewtonRaphson(int opcion, double x0, int maxIter, double tol);
+    // No implemento código ya que es un método nativo
+}
